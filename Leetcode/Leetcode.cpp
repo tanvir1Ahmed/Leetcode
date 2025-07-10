d73fbbd2f7023555a53d654f194aafb250880ad8
tanvir1Ahmed <tanvir.ahmed.ta.khan@gmail.com>
2025-07-10 17:18:36 +0600

    Add project files.

---
+++ Leetcode/Leetcode.cpp
@@ -0,0 +1,20 @@
+#include <iostream>  
+#include <vector>  
+#include <algorithm>  
+#include <cmath>  
+#include <map>  
+#include <set>  
+#include <queue>  
+#include <stack>  
+#include <string>  
+#include <cstring>  
+#include <climits>  
+#include <cstdlib>  
+#include <numeric>  
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	cout << n;
+}
